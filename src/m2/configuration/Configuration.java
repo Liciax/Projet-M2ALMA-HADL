95cacf3b96fc21a43dfaa62489f46b958c02582f
Liciax <aboubouille@gmail.com>
2016-10-17 17:17:11 +0200

    debut d'ajout des definitions des méthodes configuration et composant

--- src/m2/configuration/Configuration.java
+++ src/m2/configuration/Configuration.java
@@ -7,4 +7,12 @@ public abstract class Configuration {
 	public void notifier(PortSortie p) {
 		// TODO implementer
 	}
+	
+	public void ajouterComposant(ComposantAbstrait ca) {
+		
+	}
+	
+	public void retirerComposant(ComposantAbstrait ca) {
+		
+	}
 }
--- src/m2/configuration/composant/ComposantSimple.java
+++ src/m2/configuration/composant/ComposantSimple.java
@@ -1,5 +1,13 @@
 package m2.configuration.composant;
 
+import java.util.List;
+
+import m2.configuration.interfaces.InterfaceAPort;
+
 public abstract class ComposantSimple {
 
+		private ContrainteTechnique ct;
+		private List<Propriete> proprietes;
+		private InterfaceAPort entree;
+		private InterfaceAPort sorti;
 }
