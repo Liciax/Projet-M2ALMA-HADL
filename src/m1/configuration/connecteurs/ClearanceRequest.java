0772a07f55ab3a4843ce2c4e023ccb244a293200
XyLnEn <le_pingouin_dbil@hotmail.fr>
2016-10-23 15:50:04 +0200

    synchronization ok

--- src/m1/configuration/connecteurs/ClearanceRequest.java
+++ src/m1/configuration/connecteurs/ClearanceRequest.java
@@ -13,11 +13,11 @@ public class ClearanceRequest extends Connecteur {
 	public ClearanceRequest(ObserveurdeTransit obs) {
 		super(TypeConnecteur.EXPLICITE, new GlueClearanceRequest(), new InterfaceARoleConcret(), new InterfaceARoleConcret());
 		
-		this.getFrom().getRoles().add(new RoleEntreeConcret("CalledConnec"));//caller, requis
-		this.getFrom().getRoles().add(new RoleEntreeConcret("CalledSecu"));//caller, requis
+		this.getFrom().getRoles().add(new RoleEntreeConcret("CalledConnec"));//called, requis
+		this.getFrom().getRoles().add(new RoleEntreeConcret("CalledSecu"));//called, requis
 		
-		this.getTo().getRoles().add(new RoleSortieConcret("CallerConnec",obs));//called, fourni
-		this.getTo().getRoles().add(new RoleSortieConcret("CallerSecu",obs));//called, fourni
+		this.getTo().getRoles().add(new RoleSortieConcret("CallerConnec",obs));//caller, fourni
+		this.getTo().getRoles().add(new RoleSortieConcret("CallerSecu",obs));//caller, fourni
 	}
 	
 	public void lancer(String p){
