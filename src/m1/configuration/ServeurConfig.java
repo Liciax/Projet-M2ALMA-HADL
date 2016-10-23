0b588c31f9f524d3dd28c62ec498a8b6d0280160
Liciax <aboubouille@gmail.com>
2016-10-23 16:23:43 +0200

    ajout dans serveurconfig

--- src/m1/configuration/ServeurConfig.java
+++ src/m1/configuration/ServeurConfig.java
@@ -1,5 +1,8 @@
 package m1.configuration;
 
+import m1.configuration.ComposantsSimples.Client;
+import m1.configuration.ComposantsSimples.ConnectionManager;
+import m1.configuration.ComposantsSimples.Database;
 import m1.configuration.interfaces.port.PortEntreeConcret;
 import m1.configuration.interfaces.port.PortSortieConcret;
 import m2.configuration.Configuration;
@@ -14,7 +17,14 @@ public class ServeurConfig extends Configuration {
 		this.getInterfConf().getPorts().add(new PortEntreeConcret("EntreeConfServ"));
 		this.getInterfConf().getPorts().add(new PortSortieConcret("SortieConfServ",observServ));
 		
-		//TODO : Partie basse de m1
+		ConnectionManager ConnMana = new ConnectionManager(observServ);
+		this.listeComposants.add(ConnMana);
+		
+		Database db = new Database(observServ);
+		this.listeComposants.add(db);
+		
+		SecurityManager secuMana = new SecurityManager(observServ);
+		this.listeComposants.add(secuMana);
 	}
 
 	public void lancer(String p){
